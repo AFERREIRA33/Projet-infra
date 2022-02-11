995bb39bcf8b58817cc3a258e53db56b8c81bcfc
LGARRABOS <92851982+LGARRABOS@users.noreply.github.com>
2022-02-11 09:16:36 +0100

    Add files via upload

---
+++ creation_serveur/script_stop.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+opt=true
+
+usage() {
+        echo "Usage : MC_Start.sh [OPTION]
+        Stop the chosen server or all of them
+
+        -s      Start one particular server
+        -h      Print help message (this message)"
+}
+
+while getopts ":hs:" option; do
+        case "${option}" in
+                h)
+                        usage
+                        exit 0
+                        ;;
+                s)
+                        opt=false
+                        name=$2
+                        ;;
+                *)
+                        log error "Option ${option} not recognized."
+                        usage
+                        exit 1
+                        ;;
+        esac
+done
+
+if [ "$opt" = true ]; then
+        killserv=$(ps -ef | grep uwu2 | cut -c10-16 | head -1)
+        kill -KILL $killserv
+elif [ "$opt" = false ]; then
+        killserv=$(ps -ef | grep "$name" | cut -c10-16 | head -1)
+        kill -KILL $killserv
+fi
