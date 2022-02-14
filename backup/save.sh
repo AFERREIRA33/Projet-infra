75b11024a80c9a9ed65e8821c4d81eaee253aaac
JSELVA <joseph.selva@ynov.com>
2022-02-14 23:40:39 +0100

    doc projet_infra

--- backup/save.sh
+++ backup/save.sh
@@ -4,20 +4,24 @@ log_date=$(date +"[%y/%m/%d %H:%M:%S]")
 file_date=$(date +"%y%m%d_%H%M%S")
 backup_path="/srv/backup"
 archive_path="${backup_path}/mc_${file_date}"
-file_path="${backup_path}/mc/"
+file_path="${backup_path}/mc"
 log_path="/var/log/backup"
 log_file="${log_path}/backup.log"
-server="<user@ip>:/Minecraft"
+server="serv_install@10.200.1.5:/Minecraft"
 line="Backup ${archive_path} created successfully."
 line_error="[ ERROR ] Backup ${archive_path} was not created!"
 
-if [ -d "${backup_path}" ]
+if [ -d "$backup_path" ]
 then
-        mkdir "${backup_path}"
+        echo "already exist"
+else
+        mkdir "$backup_path"
 fi
 
 if [ -f "${file_path}" ]
 then
+        echo "already exist"
+else
         mkdir "${file_path}"
 fi
 
